022b201b59ae420696a724c21b7eb26c6a95dc5d
Starlet-web <mgbhaukali@gmail.com>
2026-07-29 13:08:29 +0530

    Create the-heaven-point.vercel.app/sw.js

---
+++ the-heaven-point.vercel.app/sw.js
@@ -0,0 +1,10 @@
+<!-- PushAlert Unified Code -->
+<script type="text/javascript">
+    (function(d, t) {
+        var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+        g.src = "https://cdn.pushalert.co/unified_582bead93220c28d219446afda6f87cf.js";
+        s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+</script>
+<!-- End PushAlert Unified Code -->
